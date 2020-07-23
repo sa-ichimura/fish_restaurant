a334f33935b2e012a8017969898a9f5132da4232
sa-ichimura <qwe.098.zxc@icloud.com>
2020-07-23 21:59:17 +0900

    比較ページ削除ロジック

--- backend/resources/views/comparison/index.blade.php
+++ backend/resources/views/comparison/index.blade.php
@@ -1,15 +1,46 @@
 @extends('layouts.common')
 @section('content')
+<div class="row">
+
     @foreach ($componentGraphDatas as $componentGraphData)
-        <piechart-component :graph-data="@json($componentGraphData['graphData'])"></piechart-component>
-        <p>{{ $componentGraphData['food']->price }}</p>
-        <p>{{ $componentGraphData['food']->name }}</p>
-        <p>{{ $componentGraphData['food']->volume }}</p>
-        <p>{{ $componentGraphData['food']->size }}</p>
-        <p>{{ $componentGraphData['food']->material }}</p>
-        <p>{{ $componentGraphData['food']->material }}</p>
-        <p>{{ $componentGraphData['food']->feature }}</p>
-        <p>{{ $componentGraphData['food']->target }}</p>
+    <div class="col-lg-4 col-sm-6 ">
+            <piechart-component :graph-data="@json($componentGraphData['graphData'])"></piechart-component>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th scope="row">対象</th>
+                        <td><p>{{ $componentGraphData['food']->target }}</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">価格</th>
+                        <td><p>{{ $componentGraphData['food']->price }}</p></td>
+                    </tr>
+                    <tr>
+                        <th>商品名</th>
+                        <td><p>{{ $componentGraphData['food']->name }}</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">内容量</th>
+                        <td><p>{{ $componentGraphData['food']->volume }}</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">サイズ</th>
+                        <td><p>{{ $componentGraphData['food']->size }}</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">原材料</th>
+                        <td><p>{{ $componentGraphData['food']->material }}</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">特徴</th>
+                        <td><p>{{ $componentGraphData['food']->feature }}</p></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+  
     @endforeach
+</div>
 
 @endsection
