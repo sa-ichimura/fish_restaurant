76892c1d850b9b3337604d68cc9c9e25f31ceb84
sa-ichimura <qwe.098.zxc@icloud.com>
2020-07-23 15:10:34 +0900

    比較ページ表示データ取得

--- backend/app/Http/Controllers/ComparisonController.php
+++ backend/app/Http/Controllers/ComparisonController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Model\Comparison;
 use App\Model\FoodComponent;
+use App\Model\Food;
 use App\User;
 
 class ComparisonController extends Controller
@@ -19,12 +20,26 @@ class ComparisonController extends Controller
 
         foreach($comparisons as $comparison){
             $foodComponents[] = FoodComponent::find($comparison->food_id);
+            $foods[] = Food::find($comparison->food_id);
         }
        
         $FoodComponentController = app()->make('App\Http\Controllers\FoodComponentController');
         $graphDatas = $FoodComponentController->componentJson( $foodComponents );
+
+
+        foreach($foodComponents as $key =>$foodComponent){
+            $componentGraphDatas[$key]['foodComponent'] = $foodComponent;
+            $componentGraphDatas[$key]['graphData'] =  $graphDatas[$key]; 
+            $componentGraphDatas[$key]['food'] =  $foods[$key]; 
+
+
+        }
+        dump($componentGraphDatas);
+
         return view('comparison/index',[
-            'test'=>'wwww'
+            'graphDatas'=>$graphDatas,
+            'foodComponents'=>$foodComponents,
+            'componentGraphDatas'=>$componentGraphDatas
         ]);
     }
 
--- backend/resources/views/comparison/index.blade.php
+++ backend/resources/views/comparison/index.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.common')
 @section('content')
+    @foreach ($componentGraphDatas as $componentGraphData)
+        <piechart-component :graph-data="@json($componentGraphData['graphData'])"></piechart-component>
+        <p>{{ $componentGraphData['food']->price }}</p>
+        <p>{{ $componentGraphData['food']->name }}</p>
+        <p>{{ $componentGraphData['food']->volume }}</p>
+        <p>{{ $componentGraphData['food']->size }}</p>
+        <p>{{ $componentGraphData['food']->material }}</p>
+        <p>{{ $componentGraphData['food']->material }}</p>
+        <p>{{ $componentGraphData['food']->feature }}</p>
+        <p>{{ $componentGraphData['food']->target }}</p>
+    @endforeach
 
-    
-        
-    {{ $test }}
 @endsection
