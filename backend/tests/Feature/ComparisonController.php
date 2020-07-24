278cc07c3d4dc0dcf3d95098e9a150f037221dd3
sa-ichimura <qwe.098.zxc@icloud.com>
2020-07-25 00:59:36 +0900

    比較ページのHTTPテスト作成

--- backend/tests/Feature/ComparisonController.php
+++ backend/tests/Feature/ComparisonController.php
@@ -4,7 +4,9 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
+use App\User;
 
 class ComparisonController extends TestCase
 {
@@ -13,10 +15,24 @@ class ComparisonController extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function test_not_login()
     {
-        $response = $this->get('/comparison');
-
-        $response->assertStatus(200);
+        //未ログインの場合loginページに遷移
+        $notLoginResponse = $this->get('/comparison')->assertRedirect('/login');
+        $notLoginResponse->assertStatus(302);
     }
+
+    /**
+     * ログイン済みの場合比較ページの表示を行う
+     */
+    public function test_login()
+    {
+        $user = factory(User::class)->create();
+        $loginResponse = $this->actingAs($user)
+        ->withSession(['foo' => 'bar'])
+        ->get('/comparison');
+        $loginResponse->assertStatus(200);
+    }
+
+    
 }
